9ab0f52b57793da4b35455b5de38e72067ad4690
Apurva Shastry <apurva.shastry@gmail.com>
2017-03-12 19:50:44 -0700

    Tooltip updated

--- react-client/src/components/GridData.jsx
+++ react-client/src/components/GridData.jsx
@@ -4,14 +4,18 @@ import { OverlayTrigger } from 'react-bootstrap';
 import { Tooltip } from 'react-bootstrap';
 
 
-const tt =(
+const tt =({card}) => (
   <Tooltip placement="top" className="in" id="tooltip-top">Something here</Tooltip>
 );
 
 
 const GridData = ({card}) => (
   <td>
-    <OverlayTrigger placement="top" overlay={tt}>
+    <OverlayTrigger placement="top" overlay={
+    	<Tooltip placement="top" className="in" id="tooltip-top">
+        {card.name} : {card.description}  - Elixir Cost : {card.elixirCost}
+    	</Tooltip>
+    }>
       <img src = {`http://www.clashapi.xyz/images/cards/${card.idName}.png`} />
     </OverlayTrigger>
   </td>
